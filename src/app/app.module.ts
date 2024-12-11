1099223a133a740df65f6b627ea8edf9e9e26216
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2024-12-11 23:56:22 +0530

    Event_binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
-import { ChangeeventComponent } from './changeevent/changeevent.component';
-import { ChangeeventdivsComponent } from './changeeventdivs/changeeventdivs.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventBindingComponent,
-    ChangeeventComponent,
-    ChangeeventdivsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EventBindingComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
